a7cbab61e0220eca1d9ae42de8ed808f1e75f35c
bayrktlihn <bayraktaralihan1995@gmail.com>
2024-05-02 21:12:40 +0300

    add some soap client methods and fix generated sources directory

--- src/main/java/io/bayrktlihn/soapmaven/SoapClient.java
+++ src/main/java/io/bayrktlihn/soapmaven/SoapClient.java
@@ -6,10 +6,15 @@ import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class SoapClient extends WebServiceGatewaySupport {
 
-    public <TRESPONSE, TREQUEST> TRESPONSE callSoapService(String location, String soapAction, TREQUEST request) {
+    public <TRESPONSE, TREQUEST> TRESPONSE callSoapServiceWithLocation(String location, String soapAction, TREQUEST request) {
         WebServiceTemplate webServiceTemplate = getWebServiceTemplate();
         SoapActionCallback soapActionCallback = new SoapActionCallback(soapAction);
         return (TRESPONSE) webServiceTemplate.marshalSendAndReceive(location, request, soapActionCallback);
+    }
+
+    public <TRESPONSE, TREQUEST> TRESPONSE callSoapServiceWithLocation(String location, TREQUEST request) {
+        WebServiceTemplate webServiceTemplate = getWebServiceTemplate();
+        return (TRESPONSE) webServiceTemplate.marshalSendAndReceive(location, request);
     }
 
 
@@ -17,5 +22,11 @@ public class SoapClient extends WebServiceGatewaySupport {
         WebServiceTemplate webServiceTemplate = getWebServiceTemplate();
         SoapActionCallback soapActionCallback = new SoapActionCallback(soapAction);
         return (TRESPONSE) webServiceTemplate.marshalSendAndReceive(request, soapActionCallback);
+    }
+
+
+    public <TRESPONSE, TREQUEST> TRESPONSE callSoapService( TREQUEST request) {
+        WebServiceTemplate webServiceTemplate = getWebServiceTemplate();
+        return (TRESPONSE) webServiceTemplate.marshalSendAndReceive(request);
     }
 }
